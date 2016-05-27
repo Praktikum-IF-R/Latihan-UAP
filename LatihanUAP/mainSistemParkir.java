62ece12c78d4becbdfb6733ce99e0e41e70c8574
Rikzapriyudhahirdad <rikza79@gmail.com>
2016-05-28 01:35:08 +0700

    155150201111320_RikzaPriyudhaHirdad

---
+++ LatihanUAP/mainSistemParkir.java
@@ -0,0 +1,20 @@
+package LatihanUAP;
+
+import java.util.Date;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class mainSistemParkir {
+
+    public static void main(String[] args) {
+        SistemParkir sp = new SistemParkir();
+        sp.setTitle("Sistem Parkir");
+        sp.setBackground(new Color(0,204,204));
+        sp.show();
+
+    }
+
+}
