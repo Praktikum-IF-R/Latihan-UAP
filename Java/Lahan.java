27de1107d2584df89276c0369d9991e3d3697c4b
Ilham Harazky <ilhamharazky@rocketmail.com>
2016-05-29 00:04:31 +0700

    155150207111143_Ilham Harazki
    -

---
+++ Java/Lahan.java
@@ -0,0 +1,10 @@
+package latihanuaap;
+public abstract class Lahan {
+    public String plat;
+    public String lahanParkir;
+    
+    public Lahan(String a, String b){
+        plat = a;
+        lahanParkir = b;
+    }
+}
