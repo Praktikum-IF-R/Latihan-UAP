eef20ee59e22e0f465561f4657de8f9dd671b64a
kadektop2 <kadektop2@gmail.com>
2016-05-28 23:25:45 +0700

    155150201111328_IMadeNarendraPutra
    bebas

---
+++ src/LatihanUAP/Lahan.java
@@ -0,0 +1,10 @@
+package LatihanUAP;
+public abstract class Lahan {
+    public String plat;
+    public String lahanParkir;
+    
+    public Lahan(String a, String b){
+        plat = a;
+        lahanParkir = b;
+    }
+}
