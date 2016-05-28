d8928689cffa097b75541c26bd28f392172cd4b5
AnitaSumiati <anitasumiati2@gmail.com>
2016-05-28 23:01:12 +0700

    155150201111333_Anita Sumiati

---
+++ Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prauap;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+    public static void main(String [] arga){
+        Parkiran parkir= new Parkiran();
+        parkir.setTitle("Sistem Parkir Mobil");
+        parkir.show();
+    }
+}
