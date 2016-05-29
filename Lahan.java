2ff33a4e66e4cee882a6d07b8a7b207f5ed39e86
Hafizmaullana <hafizmaullana193@gmail.com>
2016-05-29 11:36:25 +0700

    155150201111329_HAFIZ MAULANA

---
+++ Lahan.java
@@ -0,0 +1,10 @@
+package projek;
+public abstract class Lahan {
+    public String platnomer;
+    public String lahanParkir;
+    
+    public Lahan(String a, String b){
+         lahanParkir = b;
+	platnomer = a;
+    }
+}
